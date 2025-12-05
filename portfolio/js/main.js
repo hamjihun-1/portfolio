e86b6e83ee9f74c70b7ed5a903ae3394dd71589c
hamjihun-1 <fkalsj46@gmail.com>
2025-12-05 16:50:52 +0900

    123

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-		navigation: true, /* 오른쪽에 각 페이지의 paging */
+		navigation: false, /* 오른쪽에 각 페이지의 paging */
 		navigationPosition: 'right', /* 위치 */
 		navigationTooltips: ['첫번째', '두번째', '세번째', '네번째'], /* 툴팁 */
 		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
